8424d1c8501b1316674d1d678936c7559740584f
Pratikbhoir7089 <pratikbhoir2015@gmail.com>
2025-06-24 00:18:00 +0530

    form for add new category

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace BookStore.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
