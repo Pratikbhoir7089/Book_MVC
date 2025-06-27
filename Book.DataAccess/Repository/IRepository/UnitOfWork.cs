7800e04080a3ee03f0cda5164fedc7fb90d10a73
Pratikbhoir7089 <pratikbhoir2015@gmail.com>
2025-06-28 01:31:39 +0530

    new Repository added

--- Book.DataAccess/Repository/IRepository/UnitOfWork.cs
+++ Book.DataAccess/Repository/IRepository/UnitOfWork.cs
@@ -3,24 +3,25 @@ using Book.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Book.DataAccess.Repository.IRepository
 {
-    public class CategoryRepository : Repository<Category> ,ICategoryRepository
+    public class UnitOfWork : IUnitOfWork
     {
         private ApplicationDbContext _db;
-        public CategoryRepository(ApplicationDbContext db):base(db)
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db) 
         {
             _db = db;
+            Category = new CategoryRepository(_db);
         }
-       
 
-        public void Update(Category obj)
+        public void Save()
         {
-            _db.Categories.Update(obj);
+            _db.SaveChanges();
         }
     }
 }
